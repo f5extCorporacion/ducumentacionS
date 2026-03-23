dc4384ae796d896ce86de7c974740268143d7013
Franklim Valverde <125815174+f5extCorporacion@users.noreply.github.com>
2026-03-23 15:16:08 -0500

    Update rsbuild.config.js

--- rsbuild.config.js
+++ rsbuild.config.js
@@ -1,15 +1,11 @@
-// @ts-check
 import { defineConfig } from '@rsbuild/core';
 import { pluginReact } from '@rsbuild/plugin-react';
 
-// Docs: https://rsbuild.rs/config/
 export default defineConfig({
   plugins: [pluginReact()],
-
   output: {
     distPath: {
-      root: 'dist', // carpeta que Vercel va a usar
+      root: 'dist',
     },
-    assetPrefix: './', // 👈 evita errores de rutas en producción
   },
 });
